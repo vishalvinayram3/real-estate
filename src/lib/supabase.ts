c8084b8b1b56f3c483b440531dd3b938c4409b0e
Your Name <vishalvinayram5432@gmail.com>
2025-03-02 18:37:45 +0530

    env changes due to build issue

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "https://njkdtjgciebwytrbblbt.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qa2R0amdjaWVid3l0cmJibGJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwNzQxMzEsImV4cCI6MjA1NTY1MDEzMX0.DIrObIIxxqpLZqnocdEmXRGN5xB2oAM5AxThU0SxDZ8";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
